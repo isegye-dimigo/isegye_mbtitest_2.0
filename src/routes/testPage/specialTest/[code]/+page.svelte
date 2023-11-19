b32c0b4812d47ce79e4224b259bc33aac8f3008a
mokmokahr <jbmok.workspace@gmail.com>
2023-11-19 13:49:43 +0900

    feat: Make loading page and integrate the result page

--- src/routes/testPage/specialTest/[code]/+page.svelte
+++ src/routes/testPage/specialTest/[code]/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+    import {mbtiString,mbtiCharacters} from '../../../../store.js';
+    import { goto } from '$app/navigation';
+    import SnsShare from '$lib/components/snsShare.svelte';
     import { onMount } from 'svelte';
-    import {mbtiString} from '../../../../store.js';
     export let data;
     const curIdentifyCode = data.code;
+    let characterName:string;
     onMount(
         async function getResultFromCode() {
             const response = await fetch("/api/getMbtiString",{
@@ -17,7 +20,16 @@
             });
             const data= await response.json();
             mbtiString.set(data.mbti);
+            characterName = mbtiCharacters[data.mbti].name;
         }
     );
+    const copyShareLink = ()=>{
+        navigator.clipboard.writeText(`localhost:5173/testPage/specialTest/${curIdentifyCode}`);
+        alert("공유 링크가 클립보드에 복사되었습니다!");
+    }
 </script>
-<h1>{$mbtiString}</h1>
+<h1>당신이 이세계에 간다면?</h1>
+<h1>{characterName}</h1>
+<button on:click={()=>{goto('/')}}>다시하기</button>
+<button on:click={()=>{copyShareLink();}}>링크복사</button>
+<SnsShare/>
--- src/routes/testPage/specialTest/result/+page.svelte
+++ src/routes/testPage/specialTest/result/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import SnsShare from "$lib/components/snsShare.svelte";
-    import { mbtiString,mbtiCharacters } from "../../../../store";
+    import { mbtiString} from "../../../../store";
     import { onMount } from "svelte";
     let resultShareCode:string;
     onMount(
-        async function saveMbtiString() {
+        async function storeMbtiAndLink() {
             if($mbtiString.length === 4){
                 const response = await fetch("/api/saveInfo", {
                             method: 'POST',
@@ -20,21 +19,61 @@
                         const data= await response.json();
                         resultShareCode = data.code;
                 }
+                setTimeout(()=>{goto(`/testPage/specialTest/${resultShareCode}`);},3000);
             }
             );
-    const copyShareLink = ()=>{
-        navigator.clipboard.writeText(`localhost:5173/testPage/specialTest/${resultShareCode}`);
-        alert("공유 링크가 클립보드에 복사되었습니다!");
-    }
 </script>
 
-<h1>당신이 이세계에 간다면?</h1>
-<h1>{mbtiCharacters[$mbtiString].name}</h1>
-<button on:click={()=>{goto('/')}}>다시하기</button>
-<button on:click={()=>{copyShareLink();}}>링크복사</button>
-<SnsShare/>
+<h1>이세계에서 빠져나오는 중...</h1>
+<div class="loading"><div></div><div></div></div>
 
 
 <style>
     @import '../../../../lib/style/global.css';
+    .loading {
+        display: inline-block;
+        position: relative;
+        width: 80px;
+        height: 80px;
+    }
+    .loading div {
+        position: absolute;
+        border: 4px solid #fff;
+        opacity: 1;
+        border-radius: 50%;
+        animation: loading 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+    }
+    .loading div:nth-child(2) {
+        animation-delay: -0.5s;
+    }
+    @keyframes loading {
+        0% {
+            top: 36px;
+            left: 36px;
+            width: 0;
+            height: 0;
+            opacity: 0;
+        }
+        4.9% {
+            top: 36px;
+            left: 36px;
+            width: 0;
+            height: 0;
+            opacity: 0;
+        }
+        5% {
+            top: 36px;
+            left: 36px;
+            width: 0;
+            height: 0;
+            opacity: 1;
+        }
+        100% {
+            top: 0px;
+            left: 0px;
+            width: 72px;
+            height: 72px;
+            opacity: 0;
+        }
+    }
 </style>
